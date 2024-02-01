9f02c6ec5cc572db269cad99da17efb8e9d78e45
bonvi83 <bonvi83@gmail.com>
2024-02-01 16:28:27 +0100

    add js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,10 @@
-// // DA ESERCIZIO MARKUP BASE
-
-// const slides = [
-//     {
-//         image: 'img/01.webp',
-//             title: 'Marvel\'s Spiderman Miles Morale',
-//             text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-//         }, {
-//             image: 'img/02.webp',
-//             title: 'Ratchet & Clank: Rift Apart',
-//             text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-//         }, {
-//             image: 'img/03.webp',
-//             title: 'Fortnite',
-//             text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-//         }, {
-//             image: 'img/04.webp',
-//             title: 'Stray',
-//             text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-//         }, {
-//             image: 'img/05.webp',
-//             title: "Marvel's Avengers",
-//             text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-//         }
-//     ];
-    
-//     console.log(slides);
-
-
 const { createApp } = Vue;
 
 createApp({
 data() {
     return {
         activeIndex: 0,
-        slides : [
-            {
+        slides : [{
                     image: 'img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
@@ -57,11 +27,25 @@ data() {
                 }],
     }},
 
-    methods:{
-    addActive(index) {
-        if(index==this.activeIndex){
-            return "active";
+    methods: {
+        addActive(index) {
+            this.slideActive = index;
+        },
+
+        indietro() {
+            if (this.slideActive == 0) {
+                this.addActive(this.slides.length - 1);
+            } else {
+                this.addActive(this.slideActive - 1);
+            }
+        },
+
+        avanti() {
+            if (this.slideActive >= this.slides.length - 1) {
+                this.addActive(0);
+            } else {
+                this.addActive(this.slideActive + 1);
+            }
         }
     }
-}
-}).mount('#app')
+}).mount('#app');
